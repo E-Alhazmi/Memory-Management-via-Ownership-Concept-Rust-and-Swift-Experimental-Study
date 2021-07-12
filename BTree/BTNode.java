df3b5496413924476bed636d35909e3b8878db41
E-Alhazmi <87304495+E-Alhazmi@users.noreply.github.com>
2021-07-12 14:20:38 +0300

    initial

---
+++ BTree/BTNode.java
@@ -0,0 +1,85 @@
+
+/**
+ COPYRIGHT (c) 2018 Elaf Alhazmi . All Rights Reserved.
+ Class to declare Binary Tree Abstract Data Type - Node
+ */
+
+public class BTNode {
+    BTNode left, right ;
+    int key ;
+    
+    /**
+     * Default constructor for BTNode
+     */
+    public BTNode()
+    {
+        left = null ;
+        right = null;
+        key = 0 ;
+    }
+    
+    /**
+     * Construct a Node with a value
+     * @param n is integer value that is associated with a Node
+     */
+    public BTNode (int n)
+    {
+        left = null ;
+        right = null;
+        key = n ;
+    }
+    
+    /**
+     * Set left Node reference of the current Node
+     * @param l is BTNode, linked to the left of current BTNode
+     */
+     public void setLeft (BTNode l)
+     {
+      left = l;   
+     }
+     
+     /**
+      * Set right Node reference of the current Node
+      * @param r is BTNode, linked to the right of current BTNode
+      */
+     public void setRight (BTNode r)
+     {
+         right = r;
+     }
+     
+     /**
+      * Get left Node reference of the current Node
+      * @return the left BTNode of the current BTNode
+      */
+     public BTNode getLeft ()
+     {
+         return left ;
+     }
+     
+     /**
+      * Get right Node reference  
+      * @return the right BTNode of the current BTNode
+      */
+     public BTNode getRight ()
+     {
+         return right ;
+     }
+     
+     /**
+      * Edit the integer value of a Node
+      * @param n is integer value, editing the key value of BTNode
+      */
+     public void setKey (int n)
+     {
+         key = n;
+     }
+     
+     /**
+      * Return the integer value of a Node
+      * @return key value of current BTNode
+      */
+     public int getKey ()
+     {
+         return key ;
+     }
+}
